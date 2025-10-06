d5b21584e4e47671aaa36f1342e9f636c2b971a4
𝘑𝘢𝘤𝘬 𝘞𝘪𝘻𝘢𝘳𝘥 🪄 <jackwizard50@gmail.com>
2025-10-06 18:11:49 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -20,7 +20,7 @@ class Config(object):
     AUTH_USERS = [8287604537, 8026801357, -1002667980568]
     AUTH_CHATS = [-1002667980568]
 
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "7491676613:AAFopVGvMeuc_LKo0lhPlT183ghLdKcRvWQ")
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "8268341975:AAFNOk7MCCQFZ3NgA41YHuQztYK-oxXH1dA")
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     BOT_USERNAME = get_config("BOT_USERNAME", "ZaniEncoderBot")
     
