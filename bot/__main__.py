94db75ea706ee72bab846024112154c81f5b07f2
𝘑𝘢𝘤𝘬 𝘞𝘪𝘻𝘢𝘳𝘥 🪄 <jackwizard50@gmail.com>
2025-10-06 18:10:53 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -23,7 +23,7 @@ logging.getLogger("motor").setLevel(logging.WARNING)
 
 crf.append("25")
 codec.append("libx264")
-resolution.append("854x480")
+resolution.append("1920x1080")
 preset.append("veryfast")
 audio_b.append("40k")
 
