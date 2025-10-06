92b3a55cd19e78fba8635ed092d5728a7597af8e
𝘑𝘢𝘤𝘬 𝘞𝘪𝘻𝘢𝘳𝘥 🪄 <jackwizard50@gmail.com>
2025-10-06 13:10:14 +0600

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -37,7 +37,7 @@ from pyrogram.types import ChatPermissions, InlineKeyboardMarkup, InlineKeyboard
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, UsernameNotOccupied, ChatAdminRequired, PeerIdInvalid
 
 
-os.system("wget https://graph.org/file/b414d43b95625cd0f4dbf-4e5e4a35406c3d2763.jpg -O thumb.jpg")
+os.system("wget https://graph.org/file/8fa8bc4f8ddec10892df0-5dd29f140baf642d51.jpg -O thumb.jpg")
 
 CURRENT_PROCESSES = {}
 CHAT_FLOOD = {}
