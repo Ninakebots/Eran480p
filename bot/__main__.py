b070940d1e3b3676b3658e72525b844b0170031c
𝘑𝘢𝘤𝘬 𝘞𝘪𝘻𝘢𝘳𝘥 🪄 <jackwizard50@gmail.com>
2025-10-22 19:41:44 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -21,9 +21,9 @@ from bot.commands import Command
 logging.getLogger("pymongo").setLevel(logging.WARNING)
 logging.getLogger("motor").setLevel(logging.WARNING)
 
-crf.append("24")
+crf.append("28")
 codec.append("libx264")
-resolution.append("1920x1080")
+resolution.append("854x480")
 preset.append("veryfast")
 audio_b.append("48k")
 
