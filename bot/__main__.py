180d4468a756c1018cf52a05fbcbe2d76359d865
Ninakebots <ninakebots@gmail.com>
2025-11-22 13:35:21 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -21,11 +21,11 @@ from bot.commands import Command
 logging.getLogger("pymongo").setLevel(logging.WARNING)
 logging.getLogger("motor").setLevel(logging.WARNING)
 
-crf.append("28")
+crf.append("26")
 codec.append("libx264")
-resolution.append("1920x1080")
+resolution.append("854x480")
 preset.append("veryfast")
-audio_b.append("48k")
+audio_b.append("70k")
 
 uptime = dt.now()
 
