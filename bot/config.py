1cc34509ba206d719a47fde30f6fbde6ebab7f5e
Ninakebots <ninakebots@gmail.com>
2025-12-15 06:37:45 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -20,7 +20,7 @@ class Config(object):
     AUTH_USERS = [-1003099048197, 8051409972]
     AUTH_CHATS = []
 
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "8051409972:AAHI3GRd97bi5PJLa2hR_IkR21MCQ0w5tNI")
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "8354529582:AAG0Kp1M_b4nzilYat2RKdJnXzJGJDZbFAA")
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     BOT_USERNAME = get_config("BOT_USERNAME", "ZaniEncoderBot")
     
